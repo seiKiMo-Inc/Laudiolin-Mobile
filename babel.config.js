01a68fbac3c7441f87934c4fbd6f236a0aaa6bb4
KingRainbow44 <kobedo11@gmail.com>
2023-01-07 00:40:10 -0500

    Boilerplate code

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                alias: {
+                    "@app": "./src",
+                    "@backend": "./src/backend",
+                    "@pages": "./src/ui/pages",
+                    "@components": "./src/ui/components",
+                }
+            }
+        ]
+    ]
 };
