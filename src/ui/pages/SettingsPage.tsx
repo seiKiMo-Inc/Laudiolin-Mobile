01f12c91b033306555052a75cb5c64e63e2ef4a3
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-01-09 11:43:05 +0530

    Add placeholder `SettingsPage`

---
+++ src/ui/pages/SettingsPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Text } from "react-native";
+
+import { navigate } from "@backend/navigation";
+
+class SearchPage extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <Text>this is the settings page</Text>
+                <Button title={"Go Back"} onPress={() => navigate("Home")} />
+            </>
+        );
+    }
+}
+
+export default SearchPage;
