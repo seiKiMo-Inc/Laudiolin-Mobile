b6a5a68168a4c0d2a60b0b32ecee199b46cdf567
KingRainbow44 <kobedo11@gmail.com>
2023-01-31 00:06:52 -0500

    Match natsu's design (home)

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,75 +1,85 @@
-import type { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
-import type { TrackData } from "@backend/types";
-
 import React from "react";
-import { Button, View } from "react-native";
+import { FlatList, ScrollView, View } from "react-native";
 
 import BasicText from "@components/common/BasicText";
-import SearchTrack from "@components/search/SearchTrack";
-import Controls from "@components/player/Controls";
-import BasicTextInput from "@components/common/BasicTextInput";
 
-import { playTrack } from "@backend/audio";
-import { navigate } from "@backend/navigation";
-import { doSearch } from "@backend/search";
+import { HomePageStyle } from "@styles/PageStyles";
+import { Image } from "@rneui/base";
+import Track from "@components/Track";
 
-const track: TrackData = {
-    title: "Travelogue (Global Acappella Ver.)",
-    artist: "",
-    icon: "https://app.seikimo.moe/proxy/dgfzSddP2IOxN_oOzGkytSjYkeKnbapvioYWp9oC_7vPudR3Ln2gF5Pw7SL3IMWNYyWIsDVp3d3nGqw4=w120-h120-l90-rj?from=cart",
-    url: "http://10.0.2.2:3000/download?id=n-HypaOLx_s&engine=YouTube",
-    id: "n-HypaOLx_s",
-    duration: 186
+const testTrack = {
+    title: "Hikaru Nara (Your Lie In April)",
+    artist: "Otaku",
+    icon: "https://i.scdn.co/image/ab67616d0000b273cbd6575a821e3a9bee15fc93",
+    url: "https://open.spotify.com/track/1eznJLhlnbXrXuO8Ykkhhg",
+    id: "FRR642100241",
+    duration: 117
 };
 
-interface IState {
-    info: string;
-    query: string;
-    top: TrackData | null;
+class HomePlaylist extends React.Component<any, any> {
+    render() {
+        return (
+            <View style={HomePageStyle.playlist}>
+                <Image
+                    source={require("../../../resources/images/icon.png")}
+                    style={HomePageStyle.playlistImage}
+                />
+            </View>
+        );
+    }
 }
 
-class Home extends React.Component<any, IState> {
+class Home extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
-
-        this.state = {
-            info: "This message will update with useful info.",
-            query: "hikaru nara",
-            top: null
-        };
     }
-
-    updateQuery = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
-        this.setState({ query: event.nativeEvent.text });
-    };
-
-    search = async() => {
-        this.setState({ info: `Searching for ${this.state.query}` })
-        const result = await doSearch(this.state.query);
-        this.setState({
-            top: result.top, info: `Top Title: ${result.top.title}`
-        });
-    };
-
-    playTop = async() => {
-        if (!this.state.top) return;
-        await playTrack(this.state.top);
-    };
 
     render() {
         return (
-            <View>
-                <BasicText text={this.state.info} />
-                <Button title={"Search Page"} onPress={() => navigate("Search")} />
+            <ScrollView contentContainerStyle={HomePageStyle.container}>
+                <View style={{ paddingBottom: 20 }}>
+                    <BasicText text={"Playlists"} style={HomePageStyle.header} />
+                    <FlatList
+                        style={HomePageStyle.playlists}
+                        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+                        renderItem={() => <HomePlaylist />}
+                        horizontal showsHorizontalScrollIndicator={false}
+                    />
+                </View>
 
-                <BasicTextInput default={this.state.query} />
-                <Button title={"Search"} onPress={this.search} />
-                <Button title={"Play Top Result"} onPress={this.playTop} />
+                <View style={{ paddingBottom: 20 }}>
+                    <View style={{ flexDirection: "row" }}>
+                        <BasicText text={"Downloads"} style={HomePageStyle.header} />
+                        <View style={{ justifyContent: "center" }}>
+                            <BasicText text={"More"} style={HomePageStyle.moreDownloads} />
+                        </View>
+                    </View>
 
-                <SearchTrack result={this.state.top} onClick={() => {}} />
+                    <View>
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                    </View>
+                </View>
 
-                <Controls />
-            </View>
+                <View>
+                    <View style={{ flexDirection: "row" }}>
+                        <BasicText text={"Recent Plays"} style={HomePageStyle.header} />
+                        <View style={{ justifyContent: "center" }}>
+                            <BasicText text={"More"} style={HomePageStyle.morePlays} />
+                        </View>
+                    </View>
+
+                    <View>
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                        <Track track={testTrack} padding={15} />
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 }
