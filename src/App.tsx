5f2dd1e7d411676523587e583d11b1a6f7964703
KingRainbow44 <kobedo11@gmail.com>
2023-01-31 01:00:51 -0500

    Better page hiding system

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,76 @@
-import { Component } from "react";
+import React from "react";
 import { TabView } from "@rneui/themed";
 
 import Home from "@pages/Home";
 import SearchPage from "@pages/SearchPage";
 import LoginPage from "@pages/LoginPage";
 import SettingsPage from "@pages/SettingsPage";
+import PlaylistsPage from "@pages/PlaylistsPage";
 import PlayingTrackPage from "@pages/PlayingTrackPage";
 
 import NavBar from "@components/NavBar";
 
+class Hide extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return this.props.show ? this.props.children : null;
+    }
+}
+
 interface IState {
     pageIndex: number;
     loggedIn: boolean;
+
+    showTabs: boolean;
     showPlayingTrackPage: boolean;
+    showPlaylistsPage: boolean;
 }
 
-class App extends Component<any, IState> {
+class App extends React.Component<any, IState> {
     constructor(props: any) {
         super(props);
 
         this.state = {
             pageIndex: 0,
             loggedIn: true,
-            showPlayingTrackPage: true
+
+            showTabs: true,
+            showPlayingTrackPage: false,
+            showPlaylistsPage: false
         };
     }
 
     render() {
         return this.state.loggedIn ? (
             <>
-                <TabView
-                    value={this.state.pageIndex}
-                    onChange={(i) => this.setState({ pageIndex: i })}
-                    animationType="spring"
-                    animationConfig={{ useNativeDriver: true, speed: 100 }}
-                    disableSwipe={true}
-                    containerStyle={{ backgroundColor: "#0c0f17" }}
-                >
-                    <TabView.Item>
-                        <Home />
-                    </TabView.Item>
-                    <TabView.Item>
-                        <SearchPage />
-                    </TabView.Item>
-                    <TabView.Item>
-                        <SettingsPage />
-                    </TabView.Item>
-                </TabView>
+                <Hide show={this.state.showTabs}>
+                    <TabView
+                        value={this.state.pageIndex}
+                        onChange={(i) => this.setState({ pageIndex: i })}
+                        animationType="spring"
+                        animationConfig={{ useNativeDriver: true, speed: 100 }}
+                        disableSwipe={true}
+                        containerStyle={{ backgroundColor: "#0c0f17" }}
+                    >
+                        <TabView.Item>
+                            <Home />
+                        </TabView.Item>
+                        <TabView.Item>
+                            <SearchPage />
+                        </TabView.Item>
+                        <TabView.Item>
+                            <SettingsPage />
+                        </TabView.Item>
+                    </TabView>
 
-                <NavBar pageIndex={this.state.pageIndex} setPageIndex={(i) => this.setState({ pageIndex: i })} />
+                    <NavBar pageIndex={this.state.pageIndex} setPageIndex={(i) => this.setState({ pageIndex: i })} />
+                </Hide>
 
                 <PlayingTrackPage showPage={this.state.showPlayingTrackPage} />
+                <PlaylistsPage showPage={this.state.showPlaylistsPage} />
             </>
         ) : (
             <>
