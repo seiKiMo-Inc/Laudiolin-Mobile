7f89808b91a93d7bd238f17a6c3482a850ec9c07
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-01 11:00:15 +0530

    `Controls` component

--- src/ui/components/player/Controls.tsx
+++ src/ui/components/player/Controls.tsx
@@ -1,28 +1,66 @@
 import React from "react";
+import { View } from "react-native";
 
-import Icon from "react-native-vector-icons/FontAwesome";
+import { Icon } from "@rneui/themed";
 
 interface IProps {
-
+    shuffleRepeatControl: () => void;
+    skipToPreviousControl: () => void;
+    playControl: () => void;
+    skipToNextControl: () => void;
+    makeFavoriteControl: () => void;
 }
 
-interface IState {}
 
-
-class Controls extends React.Component<IProps, IState> {
+class Controls extends React.Component<IProps, any> {
     constructor(props: IProps) {
         super(props);
-
-        this.state = {
-
-        };
     }
 
     render() {
         return (
-            <>
-                <Icon.Button name={"facebook"} />
-            </>
+            <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 30 }}>
+                <Icon
+                    name={"shuffle"}
+                    type={"material"}
+                    size={30}
+                    color={"#FFFFFF"}
+                    underlayColor={"#FFFFFF"}
+                    onPress={this.props.shuffleRepeatControl}
+                />
+                <Icon
+                    name={"skip-previous"}
+                    type={"material"}
+                    size={30}
+                    color={"#FFFFFF"}
+                    underlayColor={"#FFFFFF"}
+                    onPress={this.props.skipToPreviousControl}
+                />
+                <Icon
+                    name={"play-arrow"}
+                    type={"material"}
+                    size={30}
+                    color={"#FFFFFF"}
+                    underlayColor={"#FFFFFF"}
+                    onPress={this.props.playControl}
+                />
+                <Icon
+                    name={"skip-next"}
+                    type={"material"}
+                    size={30}
+                    color={"#FFFFFF"}
+                    underlayColor={"#FFFFFF"}
+                    onPress={this.props.skipToNextControl}
+                />
+                <Icon
+                    name={"favorite"}
+                    type={"material"}
+                    size={30}
+                    color={"#FFFFFF"}
+                    underlayColor={"#FFFFFF"}
+                    onPress={this.props.makeFavoriteControl}
+                />
+            </View>
         );
     }
 }
