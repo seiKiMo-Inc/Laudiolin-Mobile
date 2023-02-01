da45a90f73cb266c4ce579ea2f6b7c4323df72aa
KingRainbow44 <kobedo11@gmail.com>
2023-02-01 00:01:30 -0500

    Implement logging into Laudiolin

--- src/ui/pages/LoginPage.tsx
+++ src/ui/pages/LoginPage.tsx
@@ -1,33 +1,95 @@
 import React from "react";
 import { View, ImageBackground } from "react-native";
 
+import WebView, { WebViewNavigation } from "react-native-webview";
 import BasicButton from "@components/common/BasicButton";
 import BasicText from "@components/common/BasicText";
 
 import { LoginPageStyle } from "@styles/PageStyles";
 
-class SearchPage extends React.Component<any, any> {
+import { navigate } from "@backend/navigation";
+import { getLoginUrl, login } from "@backend/user";
+
+interface IState {
+    showLogin: boolean;
+    webViewUrl: string;
+}
+
+class SearchPage extends React.Component<any, IState> {
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            showLogin: false,
+            webViewUrl: ""
+        };
+    }
+
+    /**
+     * Changes the state to show the login page.
+     */
+    login(): void {
+        this.setState({
+            showLogin: true,
+            webViewUrl: getLoginUrl()
+        });
+    }
+
+    /**
+     * Sends the user to the home page.
+     */
+    ignoreLogin(): void {
+
+    }
+
+    /**
+     * Invoked when the webview site changes.
+     */
+    onSiteChange(navi: WebViewNavigation): void {
+        const { url } = navi; // Extract the URL.
+
+        // Check if the URL is the redirect URL.
+        if (url.includes("/handoff?code=")) {
+            // Pull the code from the URL.
+            const code = url.split("/handoff?code=")[1]
+                .split("&")[0];
+            // Login with the account.
+            login(code)
+                .then(() => {
+                    // Navigate to the home page.
+                    navigate("Home");
+                    this.setState({ showLogin: false });
+                })
+                .catch(console.error);
+        } else if (url.includes("?error=")) {
+            // Remove the login page.
+            this.setState({ showLogin: false });
+            return;
+        }
     }
 
     render() {
-        return (
+        return !this.state.showLogin ? (
             <>
                 <View style={LoginPageStyle.top}>
                     <BasicButton text={"Log in with Discord"}
                                  color={"#5b67af"} bold={true}
                                  width={300} height={40} radius={10}
                                  transform={"uppercase"}
+                                 press={() => this.login()}
                     />
                 </View>
 
                 <View style={LoginPageStyle.bottom}>
-                    <BasicButton text={"Continue as Guest"}
-                                 color={"#FFFFFF"} outline={"#5b67af"}
-                                 width={300} height={40} radius={10}
-                                 transform={"uppercase"} bold={true}
-                    />
+                    <View style={{ zIndex: 1 }}>
+                        <BasicButton text={"Continue as Guest"}
+                                     color={"#FFFFFF"} outline={"#5b67af"}
+                                     width={300} height={40} radius={10}
+                                     transform={"uppercase"} bold={true}
+                                     press={() => this.ignoreLogin()}
+                        />
+                    </View>
+
                     <View style={{ paddingTop: 17 }}>
                         <BasicText text={"Logging in with discord lets you create playlists, like \n" +
                             "songs, connect with friends and more!"} style={{ textAlign: "center" }} />
@@ -39,6 +101,11 @@ class SearchPage extends React.Component<any, any> {
                     style={LoginPageStyle.image}
                 />
             </>
+        ) : (
+            <WebView
+                source={{ uri: this.state.webViewUrl }}
+                onNavigationStateChange={navi => this.onSiteChange(navi)}
+            />
         );
     }
 }
