4f2c19e349e85edd3c45e5a3aa38230bb1e843d1
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-01 11:05:13 +0530

    Edit `BasicText` component (again)

--- src/ui/components/common/BasicText.tsx
+++ src/ui/components/common/BasicText.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Text, StyleProp, TextStyle, TouchableHighlight } from "react-native";
+import { View, Text, StyleProp, TextStyle, TouchableHighlight } from "react-native";
 
 import { BasicTextStyle } from "@styles/TextStyle";
 
 interface IProps {
     text: string;
+    width?: number;
+    numberOfLines?: number;
     style?: StyleProp<TextStyle>;
     press?: () => void;
 }
@@ -16,23 +18,33 @@ class BasicText extends React.Component<IProps, any> {
 
     render() {
         return this.props.press ? (
-            <TouchableHighlight
-                onPress={this.props.press}
-            >
-                <Text style={{
-                    ...BasicTextStyle.text,
-                    ...(this.props.style as object)
-                }}>
-                    {this.props.text}
-                </Text>
-            </TouchableHighlight>
-        ) : (
-            <Text style={{
-                ...BasicTextStyle.text,
-                ...(this.props.style as object)
-            }}>
-                {this.props.text}
-            </Text>
+            <View style={{ width: this.props.width }}>
+                <TouchableHighlight
+                    onPress={this.props.press}
+                >
+                    <Text
+                        style={{
+                        ...BasicTextStyle.text,
+                        ...(this.props.style as object)
+                    }}
+                        numberOfLines={this.props.numberOfLines}
+                    >
+                        {this.props.text}
+                    </Text>
+                </TouchableHighlight>
+            </View>
+                ) : (
+                <View style={{ width: this.props.width }}>
+                    <Text
+                        style={{
+                            ...BasicTextStyle.text,
+                            ...(this.props.style as object)
+                        }}
+                        numberOfLines={this.props.numberOfLines}
+                    >
+                        {this.props.text}
+                    </Text>
+                </View>
         );
     }
 }
