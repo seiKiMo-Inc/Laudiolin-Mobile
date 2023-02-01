c96c110053a97a94c303e8b8ee95cd15a63f0346
KingRainbow44 <kobedo11@gmail.com>
2023-02-01 00:19:34 -0500

    Make text clickable

--- src/ui/components/common/BasicText.tsx
+++ src/ui/components/common/BasicText.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Text, StyleProp, TextStyle } from "react-native";
+import { Text, StyleProp, TextStyle, TouchableHighlight } from "react-native";
 
 import { BasicTextStyle } from "@styles/TextStyle";
 
 interface IProps {
     text: string;
     style?: StyleProp<TextStyle>;
+    press?: () => void;
 }
 
 class BasicText extends React.Component<IProps, any> {
@@ -15,12 +16,16 @@ class BasicText extends React.Component<IProps, any> {
 
     render() {
         return (
-            <Text style={{
-                ...BasicTextStyle.text,
-                ...(this.props.style as object)
-            }}>
-                {this.props.text}
-            </Text>
+            <TouchableHighlight
+                onPress={this.props.press}
+            >
+                <Text style={{
+                    ...BasicTextStyle.text,
+                    ...(this.props.style as object)
+                }}>
+                    {this.props.text}
+                </Text>
+            </TouchableHighlight>
         );
     }
 }
