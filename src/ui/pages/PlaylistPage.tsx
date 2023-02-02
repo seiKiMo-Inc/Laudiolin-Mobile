9caeddae7a74c72eb9d04142bf50b139d81fa9b9
KingRainbow44 <kobedo11@gmail.com>
2023-02-01 22:26:25 -0500

    Hide the playlist details if the playlist is null

--- src/ui/pages/PlaylistPage.tsx
+++ src/ui/pages/PlaylistPage.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { FlatList, View, ListRenderItemInfo } from "react-native";
 
+import { Icon, Image } from "@rneui/base";
+import Hide from "@components/Hide";
 import Track from "@components/Track";
 import BasicText from "@components/common/BasicText";
+import BasicButton from "@components/common/BasicButton";
 
 import { PlaylistPageStyle } from "@styles/PageStyles";
 
-import { TrackData } from "@backend/types";
-import { Icon, Image } from "@rneui/base";
-import BasicButton from "@components/common/BasicButton";
+import { Playlist, TrackData } from "@backend/types";
+import emitter from "@backend/events";
 
 const testTrack = {
     title: "Hikaru Nara (Your Lie In April)",
@@ -23,11 +25,26 @@ interface IProps {
     showPage: boolean;
 }
 
-class PlaylistPage extends React.Component<IProps, any> {
+interface IState {
+    playlist: Playlist|null;
+}
+
+class PlaylistPage extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
+
+        this.state = {
+            playlist: null
+        };
+
+        emitter.on("showPlaylist", (playlist: Playlist) =>
+            this.setState({ playlist }));
     }
 
+    /**
+     * Renders a playlist track.
+     * @param item The track data.
+     */
     renderPlaylist(item: ListRenderItemInfo<TrackData>) {
         return (
             <Track key={item.index} track={item.item} padding={20} />
@@ -35,65 +52,70 @@ class PlaylistPage extends React.Component<IProps, any> {
     }
 
     render() {
+        // Check for a valid playlist.
+        const { playlist } = this.state;
+
         return this.props.showPage ? (
             <View style={PlaylistPageStyle.container}>
-                <View style={PlaylistPageStyle.info}>
-                    <Image
-                        source={require("../../../resources/images/icon.png")}
-                        style={PlaylistPageStyle.playlistIcon}
-                    />
-
-                    <View style={PlaylistPageStyle.text}>
-                        <BasicText
-                            text={"i'm literally losing my mind"}
-                            style={PlaylistPageStyle.playlistName}
+                <Hide show={playlist != null}>
+                    <View style={PlaylistPageStyle.info}>
+                        <Image
+                            source={{ uri: playlist?.icon }}
+                            style={PlaylistPageStyle.playlistIcon}
                         />
-                        <BasicText
-                            text={"natsu#4700"}
-                            style={PlaylistPageStyle.playlistAuthor}
+
+                        <View style={PlaylistPageStyle.text}>
+                            <BasicText
+                                text={playlist?.name ?? ""}
+                                style={PlaylistPageStyle.playlistName}
+                            />
+                            <BasicText
+                                text={playlist?.owner ?? ""}
+                                style={PlaylistPageStyle.playlistAuthor}
+                            />
+                        </View>
+                    </View>
+
+                    <View style={PlaylistPageStyle.actions}>
+                        <BasicButton
+                            right={20}
+                            text={"Edit"}
+                            color={"#1b273a"}
+                            title={PlaylistPageStyle.editText}
+                            button={PlaylistPageStyle.editButton}
+                            icon={<Icon
+                                color={"white"}
+                                type={"material"} name={"edit"}
+                                style={{ paddingRight: 10 }}
+                            />}
+                        />
+
+                        <Icon
+                            type={"material"} name={"favorite"}
+                            iconStyle={PlaylistPageStyle.favoriteIcon}
+                        />
+
+                        <BasicButton
+                            text={"Shuffle"}
+                            color={"#4e7abe"}
+                            title={PlaylistPageStyle.shuffleText}
+                            button={PlaylistPageStyle.shuffleButton}
+                            container={{ position: "absolute", right: 40 }}
+                            icon={<Icon
+                                color={"white"}
+                                type={"material"} name={"shuffle"}
+                                style={{ paddingRight: 5 }}
+                            />}
                         />
                     </View>
-                </View>
 
-                <View style={PlaylistPageStyle.actions}>
-                    <BasicButton
-                        right={20}
-                        text={"Edit"}
-                        color={"#1b273a"}
-                        title={PlaylistPageStyle.editText}
-                        button={PlaylistPageStyle.editButton}
-                        icon={<Icon
-                            color={"white"}
-                            type={"material"} name={"edit"}
-                            style={{ paddingRight: 10 }}
-                        />}
+                    <FlatList
+                        style={PlaylistPageStyle.tracks}
+                        data={[testTrack]}
+                        renderItem={this.renderPlaylist}
+                        showsHorizontalScrollIndicator={false}
                     />
-
-                    <Icon
-                        type={"material"} name={"favorite"}
-                        iconStyle={PlaylistPageStyle.favoriteIcon}
-                    />
-
-                    <BasicButton
-                        text={"Shuffle"}
-                        color={"#4e7abe"}
-                        title={PlaylistPageStyle.shuffleText}
-                        button={PlaylistPageStyle.shuffleButton}
-                        container={{ position: "absolute", right: 40 }}
-                        icon={<Icon
-                            color={"white"}
-                            type={"material"} name={"shuffle"}
-                            style={{ paddingRight: 5 }}
-                        />}
-                    />
-                </View>
-
-                <FlatList
-                    style={PlaylistPageStyle.tracks}
-                    data={[testTrack]}
-                    renderItem={this.renderPlaylist}
-                    showsHorizontalScrollIndicator={false}
-                />
+                </Hide>
             </View>
         ) : null;
     }
