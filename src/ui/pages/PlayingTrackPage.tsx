e10b99d95ec7f88d3ee82f7e7050f201ac4d7617
KingRainbow44 <kobedo11@gmail.com>
2023-02-02 01:53:35 -0500

    Handle player updates on the track page

--- src/ui/pages/PlayingTrackPage.tsx
+++ src/ui/pages/PlayingTrackPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, ImageBackground } from "react-native";
+import { View, ImageBackground } from "react-native";
 
 import { Icon, Image } from "@rneui/themed";
 import BasicText from "@components/common/BasicText";
@@ -8,36 +8,93 @@ import Controls from "@components/player/Controls";
 
 import { PlayingTrackPageStyle } from "@styles/PageStyles";
 
-import { TrackData } from "@backend/types";
-import { shuffleQueue } from "@backend/audio";
-import TrackPlayer from "react-native-track-player";
-
-const track: TrackData = {
-    title: "Travelogue (Global Acappella Ver.)",
-    artist: "HoYoVerse",
-    icon: "https://i.scdn.co/image/ab67616d0000b273e933b812f801dc6e55fda564",
-    url: "http://10.0.2.2:3000/download?id=n-HypaOLx_s&engine=YouTube",
-    id: "n-HypaOLx_s",
-    duration: 1860000
-};
-
+import { getCurrentTrack, shuffleQueue } from "@backend/audio";
+import TrackPlayer, { Event, State, Track } from "react-native-track-player";
 
 interface IProps {
     showPage: boolean;
-    showPageFn: (show: boolean) => void;
+    showPageFn: () => void;
 }
 
-class PlayingTrackPage extends React.Component<IProps, never> {
+interface IState {
+    track: Track|null;
+    position: number;
+    paused: boolean;
+}
+
+class PlayingTrackPage extends React.Component<IProps, IState> {
+    updateInterval: any|null = null;
+
     constructor(props: IProps) {
         super(props);
+
+        this.state = {
+            track: null,
+            position: 0,
+            paused: false
+        };
+
+        // Register track player listeners.
+        TrackPlayer.addEventListener(Event.PlaybackTrackChanged, () => this.update());
+        TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => this.update());
+        TrackPlayer.addEventListener(Event.PlaybackState, () => this.update());
+    }
+
+    /**
+     * Toggles the playback state of the audio player.
+     */
+    async togglePlayback(): Promise<void> {
+        // Check if the player is paused.
+        if (await TrackPlayer.getState() == State.Paused)
+            await TrackPlayer.play();
+        else
+            await TrackPlayer.pause();
+
+        // Update the component.
+        await this.update();
+    }
+
+    /**
+     * Called when the track player updates.
+     */
+    async positionUpdate() {
+        // Set the state.
+        this.setState({
+            position: await TrackPlayer.getPosition()
+        });
+    }
+
+    /**
+     * Called when the track player updates.
+     */
+    async update() {
+        const track = await getCurrentTrack();
+
+        // Set the state.
+        this.setState({
+            track: track ?? this.state.track,
+            position: await TrackPlayer.getPosition(),
+            paused: await TrackPlayer.getState() == State.Paused
+        });
+    }
+
+    componentDidMount() {
+        this.updateInterval = setInterval(() => this.positionUpdate(), 500);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.updateInterval);
     }
 
     render() {
+        let { track } = this.state;
+        if (!track) return null;
+
         return this.props.showPage ? (
                 <View style={PlayingTrackPageStyle.view}>
                     <ImageBackground
                         style={PlayingTrackPageStyle.background}
-                        source={{ uri: track.icon }}
+                        source={{ uri: track.artwork as string }}
                         blurRadius={50}
                     >
                         <View style={{ ...PlayingTrackPageStyle.background, backgroundColor: "rgba(0,0,0, 0.6)" }} />
@@ -49,60 +106,61 @@ class PlayingTrackPage extends React.Component<IProps, never> {
                             type={"material"}
                             size={30}
                             color={"#FFFFFF"}
-                            onPress={() => this.props.showPageFn(false)}
+                            onPress={() => this.props.showPageFn()}
                         />
-                        
+
                         <View style={PlayingTrackPageStyle.topBarText}>
-                            <BasicText 
+                            <BasicText
                                 text={"Playing From Playlist"}
                                 style={{ textTransform: "uppercase" }}
                             />
-                            <BasicText 
+                            <BasicText
                                 text={"Favorites"}
-                                style={{ fontWeight: "bold", color: "#FFFFFF" }} 
+                                style={{ fontWeight: "bold", color: "#FFFFFF" }}
                             />
                         </View>
-                        
+
                         <Icon
                             name={"more-vert"}
                             type={"material"}
                             size={30}
                             color={"#FFFFFF"}
-                            onPress={() => console.log("TODO: Show more options") /* TODO: Make more-vert menu */}
+                            onPress={() => null /* TODO: Make more-vert menu */}
                         />
                     </View>
 
                     <View style={PlayingTrackPageStyle.trackInfo}>
                         <Image
                             style={PlayingTrackPageStyle.trackImage}
-                            source={{ uri: track.icon }}
+                            source={{ uri: track.artwork as string }}
                         />
-                        
+
                         <View style={{ padding: 25, flexDirection: "column", gap: 10 }}>
-                            <BasicText 
-                                numberOfLines={2} 
-                                style={{ color: "#FFFFFF", fontSize: 25 }} 
-                                text={track.title}
+                            <BasicText
+                                numberOfLines={2}
+                                style={{ color: "#FFFFFF", fontSize: 25 }}
+                                text={track.title ?? ""}
                             />
-                            <BasicText 
-                                numberOfLines={1} 
-                                style={{ color: "#a1a1a1", fontSize: 15 }} 
-                                text={track.artist}
+                            <BasicText
+                                numberOfLines={1}
+                                style={{ color: "#a1a1a1", fontSize: 15 }}
+                                text={track.artist ?? ""}
                             />
 
-                            <ProgressBar 
-                                trackLength={track.duration} 
-                                currentTime={track.duration / 2} 
-                                onSeek={() => null} 
-                                onSlidingStart={() => null} 
+                            <ProgressBar
+                                trackLength={track.duration ?? 0}
+                                currentTime={this.state.position}
+                                onSeek={time => TrackPlayer.seekTo(time)}
+                                onSlidingStart={() => null}
                             />
 
                             <Controls
-                                shuffleRepeatControl={async () => await shuffleQueue()}
-                                skipToPreviousControl={async () => await TrackPlayer.skipToPrevious()}
-                                playControl={async () => await TrackPlayer.play()}
-                                skipToNextControl={async () => await TrackPlayer.skipToNext()}
-                                makeFavoriteControl={async () => null} />
+                                isPaused={this.state.paused}
+                                shuffleRepeatControl={() => shuffleQueue()}
+                                skipToPreviousControl={() => TrackPlayer.skipToPrevious()}
+                                playControl={async () => this.togglePlayback()}
+                                skipToNextControl={() => TrackPlayer.skipToNext()}
+                                makeFavoriteControl={() => null} />
                         </View>
                     </View>
                 </View>
