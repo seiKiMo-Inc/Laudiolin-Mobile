fd6199b4d8d4bf78543cd2a8ba4b58654e54919a
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-02-05 15:45:26 +0530

    Create `BasicInput`

---
+++ src/ui/components/common/BasicInput.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleProp,  ViewStyle, TextStyle } from "react-native";
+
+import { Input } from "@rneui/themed";
+
+interface IProps {
+    multiline?: boolean;
+    numberOfLines?: number;
+    ref?: React.RefObject<any>;
+    placeholder?: string;
+    onChangeText: (text: string) => void;
+    containerStyle?: StyleProp<ViewStyle>;
+    inputStyle?: StyleProp<TextStyle>;
+    label?: string;
+}
+
+class BasicInput extends React.Component<IProps, any> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Input
+                label={this.props.label ?? ""}
+                ref={this.props.ref ?? null}
+                placeholder={this.props.placeholder ?? ""}
+                onChangeText={this.props.onChangeText}
+                inputContainerStyle={{
+                    ...this.props.containerStyle as object,
+                    width: "100%",
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    borderColor: "#12207e",
+                    paddingRight: 10,
+                    paddingLeft: 10,
+                }}
+                labelStyle={{ padding: 10 }}
+                inputStyle={{ ...this.props.inputStyle as object, color: "#ffffff" }}
+                multiline={this.props.multiline ?? false}
+                numberOfLines={this.props.numberOfLines ?? 1}
+            />
+        );
+    }
+}
+
+export default BasicInput;
