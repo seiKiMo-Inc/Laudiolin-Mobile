707ea7ba1e41674c68cd047da0f821abaa929f59
KingRainbow44 <kobedo11@gmail.com>
2023-02-04 21:43:43 -0500

    Add basic settings implementation

--- src/ui/pages/SettingsPage.tsx
+++ src/ui/pages/SettingsPage.tsx
@@ -8,8 +8,57 @@ import { Image } from "@rneui/base";
 
 import { SettingsPageStyle } from "@styles/PageStyles";
 
-import { User } from "@backend/types";
+import * as settings from "@backend/settings";
 import { logout, userData } from "@backend/user";
+import type { User, SettingType } from "@backend/types";
+
+class Setting extends React.Component<
+    { setting: string, type: SettingType, options?: string[] }, { value: string }
+> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            value: ""
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({ value: await settings.getFromPath(
+            this.props.setting) ?? "" });
+    }
+
+    async showInput() {
+        switch(this.props.type) {
+            case "select":
+                // Get the select options.
+                const { options } = this.props;
+                if (options == null) return;
+
+                // Get the index of the current select option.
+                const index = options.findIndex(option => option == this.state.value);
+                if (index == -1) return;
+                // Get the next option, or the first option if there is no next option.
+                const next = options[index + 1] ?? options[0];
+                // Set the next option.
+                await settings.saveFromPath(this.props.setting, next);
+                this.setState({ value: next });
+                return;
+        }
+    }
+
+    render() {
+        return (
+            <View style={SettingsPageStyle.configure}>
+                <BasicText text={settings.settingsKeys[this.props.setting]}
+                           style={SettingsPageStyle.setting} />
+                <BasicText text={this.state.value}
+                           style={SettingsPageStyle.value}
+                           press={() => this.showInput()} />
+            </View>
+        );
+    }
+}
 
 interface IState {
     user: User|null;
@@ -63,29 +112,14 @@ class SearchPage extends React.Component<any, IState> {
                 </View>
 
                 <View style={SettingsPageStyle.settingsContainer}>
-                    {/* TODO: Replace with a forEach loop. */}
                     <View style={{ paddingBottom: 20 }}>
                         <BasicText
                             text={"General"}
                             style={SettingsPageStyle.category}
                         />
 
-                        {/* TODO: Replace with a forEach loop. */}
-                        <View style={SettingsPageStyle.configure}>
-                            <BasicText text={"Downloads Folder"} style={SettingsPageStyle.setting} />
-                            <BasicText text={"C:\\Users\\natsu\\Downloads"} style={SettingsPageStyle.value} />
-                        </View>
-
-                        <View style={SettingsPageStyle.configure}>
-                            <BasicText text={"Preferred Search Engine"} style={SettingsPageStyle.setting} />
-                            <BasicText text={"YouTube"} style={SettingsPageStyle.value} />
-                        </View>
-                    </View>
-                    <View style={{ paddingBottom: 20 }}>
-                        <BasicText
-                            text={"UI Settings"}
-                            style={SettingsPageStyle.category}
-                        />
+                        <Setting setting={"search.engine"} type={"select"}
+                                 options={["All", "YouTube", "Spotify"]} />
                     </View>
                 </View>
             </ScrollView>
