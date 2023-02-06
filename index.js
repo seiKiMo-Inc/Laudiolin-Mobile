cb7a7783118e989b508e4453f5868e201f0bc3a0
KingRainbow44 <kobedo11@gmail.com>
2023-02-06 02:05:02 -0500

    Add logging methods on `Promise`s

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import App from "@app/App";
 import { setAppData } from "@app/constants";
 import { playbackService } from "@backend/audio";
 
+import { console } from "@app/utils";
+
 // Setup event listeners.
 import * as fs from "@backend/fs";
 import * as gateway from "@backend/gateway";
@@ -26,17 +28,23 @@ async function initialize() {
     setAppData(app); // Set the app data.
 
     // Fetch the application settings.
-    await settings.reloadSettings();
+    settings.reloadSettings()
+        .catch(err => console.error("Failed to load settings.", err));
     // Create folders if needed.
-    await fs.createFolders();
+    fs.createFolders()
+        .catch(err => console.error("Failed to create folders.", err));
 
     // Initialize the gateway.
-    await gateway.setupListeners();
-    await gateway.connect()
+    gateway.setupListeners();
+    gateway.connect();
 
-    // Initialize the track player.
-    await TrackPlayer.setupPlayer();
-    await TrackPlayer.updateOptions({
-        progressUpdateEventInterval: 1
-    });
+    try {
+        // Initialize the track player.
+        await TrackPlayer.setupPlayer();
+        await TrackPlayer.updateOptions({
+            progressUpdateEventInterval: 1
+        });
+    } catch (err) {
+        console.error("Failed to initialize the track player.", err);
+    }
 }
