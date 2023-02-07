291ec893aadf3e9b4a02300fa84be47d2c31abc4
KingRainbow44 <kobedo11@gmail.com>
2023-02-07 00:19:16 -0500

    Fix the logout button

--- src/ui/styles/PageStyles.ts
+++ src/ui/styles/PageStyles.ts
@@ -53,14 +53,12 @@ export const HomePageStyle = StyleSheet.create({
     morePlaylists: {
         textDecorationLine: "underline",
         textAlign: "right",
-        paddingRight: 20,
-        paddingLeft: "58%"
+        paddingRight: 20
     },
     moreDownloads: {
         textDecorationLine: "underline",
         textAlign: "right",
-        paddingRight: 20,
-        paddingLeft: "50%"
+        paddingRight: 20
     },
     morePlays: {
         textDecorationLine: "underline",
@@ -104,6 +102,7 @@ export const SettingsPageStyle = StyleSheet.create({
         paddingBottom: 20
     },
     userContainer: {
+        width: screenWidth,
         flexDirection: "row",
     },
     userImage: {
@@ -114,11 +113,14 @@ export const SettingsPageStyle = StyleSheet.create({
         fontSize: 16,
         fontWeight: "bold",
     },
+    logOutContainer: {
+        position: "absolute",
+        top: 12,
+        left: screenWidth - 90
+    },
     logOut: {
         fontSize: 13,
-        textDecorationLine: "underline",
-        textAlign: "right",
-        paddingLeft: "45%"
+        textDecorationLine: "underline"
     },
     settingsContainer: {
         paddingTop: 30
@@ -251,7 +253,7 @@ export const PlayingTrackPageStyle = StyleSheet.create({
         flex: 1,
         width: screenWidth,
         height: screenHeight,
-        maxHeight: screenHeight * 0.5,
+        maxHeight: screenHeight * 0.45,
         maxWidth: 400,
     },
     alert: {
