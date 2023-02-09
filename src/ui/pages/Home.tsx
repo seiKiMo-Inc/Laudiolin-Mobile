df486ec5722da82e8a0a53d487fd891ed1c4fbb9
KingRainbow44 <kobedo11@gmail.com>
2023-02-08 21:04:01 -0500

    Use the system font for the greeting text

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -213,7 +213,7 @@ class Home extends React.Component<any, IState> {
 
                 <BasicText
                     text={this.greetingText()}
-                    style={{ fontSize: 20, color: "white", fontFamily: "Roboto", fontWeight: "300" }}
+                    style={{ fontSize: 20, color: "white", fontFamily: "System", fontWeight: "300" }}
                     containerStyle={{ width: "100%", padding: 40, paddingTop: 60, justifyContent: "center", alignItems: "center" }}
                 />
 
