30d515224fbac81296acbbfb718e99b28c8af94e
KingRainbow44 <kobedo11@gmail.com>
2023-02-15 23:37:41 -0500

    Allow for the switching between development and production routes

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,36 @@
 import { updateTargetRoute } from "@backend/user";
+import { Platform } from "react-native";
 
-let app: any = {
-    encrypted: false,
-    address: "10.0.2.2",
-    port: 3000
+type AppData = {
+    dev: boolean;
+
+    encrypted: boolean;
+    address: string;
+    port: number;
+
+    localEncrypted: boolean;
+    localAddress: string;
+    localPort: number;
+
+    adbEncrypted: boolean;
+    adbAddress: string;
+    adbPort: number;
+}
+
+let app: AppData = {
+    dev: false,
+
+    encrypted: true,
+    address: "app.seikimo.moe",
+    port: 443,
+
+    localEncrypted: false,
+    localAddress: "192.168.1.2",
+    localPort: 3000,
+
+    adbEncrypted: false,
+    adbAddress: "10.0.2.2",
+    adbPort: 3000,
 };
 
 /**
@@ -14,8 +41,17 @@ export function setAppData(data: any): void {
     app = data;
 
     // Update the gateway.
-    Gateway.url = `${app.encrypted ? "https:" : "http:"}//${app.address}:${app.port}`;
-    Gateway.socket = `${app.encrypted ? "wss:" : "ws:"}//${app.address}:${app.port}`;
+    if (!app.dev) {
+        Gateway.url = `${app.encrypted ? "https:" : "http:"}//${app.address}:${app.port}`;
+        Gateway.socket = `${app.encrypted ? "wss:" : "ws:"}//${app.address}:${app.port}`;
+    } else {
+        let encrypted = Platform.OS == "ios" ? app.localEncrypted : app.adbEncrypted;
+        let address = Platform.OS == "ios" ? app.localAddress : app.adbAddress;
+        let port = Platform.OS == "ios" ? app.localPort : app.adbPort;
+
+        Gateway.url = `${encrypted ? "https:" : "http:"}//${address}:${port}`;
+        Gateway.socket = `${encrypted ? "wss:" : "ws:"}//${address}:${port}`;
+    }
 
     // Update gateway dependencies.
     updateTargetRoute();
