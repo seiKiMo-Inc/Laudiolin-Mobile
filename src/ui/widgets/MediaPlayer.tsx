8e105ff34a6174e61903bfb764359ae1e4de7aab
KingRainbow44 <kobedo11@gmail.com>
2024-07-28 21:48:21 -0400

    feat(MediaPlayer.tsx): Add gestures to component

--- src/ui/widgets/MediaPlayer.tsx
+++ src/ui/widgets/MediaPlayer.tsx
@@ -1,10 +1,11 @@
-import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 import MdIcon from "react-native-vector-icons/MaterialIcons";
 import McIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import FastImage from "react-native-fast-image";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { Directions, Gesture, GestureDetector } from "react-native-gesture-handler";
 import TrackPlayer, { State, useActiveTrack, usePlaybackState } from "react-native-track-player";
 
 import StyledText, { Size } from "@components/StyledText";
@@ -21,52 +22,63 @@ function MediaPlayer() {
     const track = useActiveTrack();
     const { state } = usePlaybackState();
 
+    const clearQueue = () => TrackPlayer.stop();
+    const openQueue = () => navigation.navigate("Queue");
+    const queueGesture = Gesture.Fling()
+        .direction(Directions.UP)
+        .onEnd(openQueue);
+    const clearGesture = Gesture.Fling()
+        .direction(Directions.DOWN)
+        .onEnd(clearQueue);
+
     return track ? (
-        <View style={style.MediaPlayer}>
-            <FastImage
-                style={style.MediaPlayer_Image}
-                source={{ uri: track.artwork }}
-                resizeMode={"cover"}
-            >
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    style={style.MediaPlayer_Background}
-                    onPress={() => global.setShowTrackPage(true)}
+        <GestureDetector gesture={Gesture.Exclusive(queueGesture, clearGesture)}>
+            <View style={style.MediaPlayer}>
+                <FastImage
+                    style={style.MediaPlayer_Image}
+                    source={{ uri: track.artwork }}
+                    resizeMode={"cover"}
                 >
-                    <View style={{
-                        ...style.MediaPlayer_Info,
-                        width: `${settings.ui.show_queue ? "65" : "75"}%`
-                    }}>
-                        <StyledText style={style.MediaPlayer_Text} text={track.title ?? "---"} size={Size.Text} />
-                        <StyledText style={style.MediaPlayer_Text} text={artist(track.artist)} size={Size.Footnote} />
-                    </View>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={style.MediaPlayer_Background}
+                        onPress={() => global.setShowTrackPage(true)}
+                    >
+                        <View style={{
+                            ...style.MediaPlayer_Info,
+                            width: `${settings.ui.show_queue ? "65" : "75"}%`
+                        }}>
+                            <StyledText style={style.MediaPlayer_Text} text={track.title ?? "---"} size={Size.Text} />
+                            <StyledText style={style.MediaPlayer_Text} text={artist(track.artist)} size={Size.Footnote} />
+                        </View>
 
-                    <View style={style.MediaPlayer_Controls}>
-                        { settings.ui.show_queue && (
-                            <TouchableOpacity onPress={() => navigation.navigate("Queue")}>
-                                <MdIcon name={"queue-music"} size={32} color={"white"} />
+                        <View style={style.MediaPlayer_Controls}>
+                            { settings.ui.show_queue && (
+                                <TouchableOpacity onPress={() => navigation.navigate("Queue")}>
+                                    <MdIcon name={"queue-music"} size={32} color={"white"} />
+                                </TouchableOpacity>
+                            ) }
+
+                            <TouchableOpacity
+                                onPress={async () => {
+                                    if (state == State.Paused) {
+                                        await TrackPlayer.play();
+                                    } else if (state == State.Playing) {
+                                        await TrackPlayer.pause();
+                                    }
+                                }}
+                            >
+                                <MdIcon name={state == State.Playing ? "pause" : "play-arrow"} size={32} color={"white"} />
                             </TouchableOpacity>
-                        ) }
 
-                        <TouchableOpacity
-                            onPress={async () => {
-                                if (state == State.Paused) {
-                                    await TrackPlayer.play();
-                                } else if (state == State.Playing) {
-                                    await TrackPlayer.pause();
-                                }
-                            }}
-                        >
-                            <MdIcon name={state == State.Playing ? "pause" : "play-arrow"} size={32} color={"white"} />
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => Player.skipToNext()}>
-                            <McIcon name={"skip-next"} size={32} color={"white"} />
-                        </TouchableOpacity>
-                    </View>
-                </TouchableOpacity>
-            </FastImage>
-        </View>
+                            <TouchableOpacity onPress={() => Player.skipToNext()}>
+                                <McIcon name={"skip-next"} size={32} color={"white"} />
+                            </TouchableOpacity>
+                        </View>
+                    </TouchableOpacity>
+                </FastImage>
+            </View>
+        </GestureDetector>
     ) : undefined;
 }
 
